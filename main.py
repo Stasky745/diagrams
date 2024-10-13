9a644d20a522262f43f26993473eca659e5b8d6a
Stasky745 <github.dev@stasky.anonaddy.me>
2024-10-13 11:10:50 +0200

    finish home_networking

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from diagrams import Diagram, Cluster, Edge
 from diagrams.generic.network import Router, Switch, Firewall
-from diagrams.generic.device import Mobile, Tablet
 from diagrams.generic.os import Windows
 from diagrams.generic.storage import Storage
 from diagrams.custom import Custom
@@ -10,49 +9,61 @@ graph_attr = {
     "compound":"true",
 }
 
-with Diagram("Home Networking", outformat="svg", show=False, direction="TB", graph_attr=graph_attr):
+with Diagram("", filename="home_networking", outformat="svg", show=False, graph_attr=graph_attr, direction="TB"):
     switch = Switch("Switch")
 
-    with Cluster("10", graph_attr={"label": "VLAN10 (TRUSTED): 192.168.1.10/24"}):
-        void = Custom("PC: 192.168.1.14", "./resources/void.png")
+    with Cluster("10", graph_attr={"label": "VLAN10 (TRUSTED): 192.168.10.0/24"}):
+        void = Custom("PC: 192.168.10.4", "./resources/void.png")
         vlan10 = [
             void,
+            Custom("WorkLaptop", "./resources/macbook.png"),
             Custom("Trusted WiFi", "./resources/ap_unifi.png")
         ]
     
-    with Cluster("42", graph_attr={"label": "VLAN42 (SERVERS): 192.168.1.42/24"}):
-        talos_m1 = Custom("M1: 192.168.1.45", "./resources/talos.svg")
-        talos_m2 = Custom("M2: 192.168.1.46", "./resources/talos.svg")
-        talos_m3 = Custom("M3: 192.168.1.47", "./resources/talos.svg")
-
-        nas = Storage("NAS/DAS")
+    with Cluster("42", graph_attr={"label": "VLAN42 (SERVERS): 192.168.42.0/24"}):
 
         with Cluster("K8s"):
             k8s = [
-                talos_m1,
-                talos_m2,
-                talos_m3,
+                Custom("\n192.168.42.10", "./resources/talos.svg"),
+                Custom("\n192.168.42.11", "./resources/talos.svg"),
+                Custom("\n192.168.42.12", "./resources/talos.svg"),
             ]
         
+        nas = Storage("NAS/DAS")
+
         vlan42 = [
+            k8s,
             nas,
-            Windows("Sunshine/Moonlight")
+            Windows("Sunshine/\nMoonlight")
         ]
     
     
-    with Cluster("50", graph_attr={"label": "VLAN50 (GUEST): 192.168.1.50/24"}):
+    with Cluster("50", graph_attr={"label": "VLAN50 (GUEST): 192.168.50.0/24"}):
         vlan50 = [
-            Mobile(""),
-            Tablet(""),
-            Custom("Guest WiFi", "./resources/ap_unifi.png")
+            Custom("Guest WiFi", "./resources/ap_unifi.png"),
         ]
 
-    #with Cluster("VLAN70 (IOT): 192.168.1.70/24"):
+    with Cluster("70", graph_attr={"label": "VLAN70 (IOT): 192.168.70.0/24"}):
+        vlan70 = [
+            Custom("FireTV", "./resources/firetv.png")
+        ]
+    
+    with Cluster("90", graph_attr={"label": "VLAN90 (CCTV): 192.168.90.0/24"}):
+        vlan90 = [
+            Custom("CCTV", "./resources/cctv.png")
+        ]
+    
+    with Cluster("100", graph_attr={"label": "VLAN100 (VPN): 192.168.100.0/24"}):
+        vlan100 = [
+            Custom("WireGuard", "./resources/wireguard.svg")
+        ]
 
     Router("ISP") >> Firewall("VyOS") >> switch
 
     switch >> Edge(lhead="cluster_10") >> vlan10[len(vlan10)//2]
     switch >> Edge(lhead="cluster_42") >> vlan42[len(vlan42)//2]
     switch >> Edge(lhead="cluster_50") >> vlan50[len(vlan50)//2]
+    switch >> Edge(lhead="cluster_70") >> vlan70[len(vlan70)//2]
+    switch >> Edge(lhead="cluster_90") >> vlan90[len(vlan90)//2]
+    switch >> Edge(lhead="cluster_100") >> vlan100[len(vlan100)//2]
 
-    k8s >> nas
